9e8e94161385b83ed9020492b4d764cc1a719389
Okan Çelebi <okancelebi06@gmail.com>
2023-02-22 21:30:50 +0300

    Add files via upload

---
+++ csharp ve operatoru/csharp ve operatoru/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ve_operatoru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //&& Ve Operatörüdür.
+            int sayi1 = 5;
+            int sayi2 = 5;
+            int sayi3 = 10;
+            bool kontrolet = sayi1 == sayi2 && sayi1 < sayi3;
+            Console.WriteLine(kontrolet);
+            Console.ReadLine();
+        }
+    }
+}
